4109ad82bf704ab16d57e6d2c369c12ae00aa9a9
Kevin Araya <kevin.araya.camacho@est.una.ac.cr>
2018-04-07 15:50:57 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/csar/kbc_news/Main.java

--- app/src/main/java/com/example/csar/kbc_news/utils/Constantes.java
+++ app/src/main/java/com/example/csar/kbc_news/utils/Constantes.java
@@ -1,8 +1,8 @@
 package com.example.csar.kbc_news.utils;
 
-/**
- * Created by Kevin on 4/7/2018.
- */
+
 
 public class Constantes {
+    public static final String NEWS_API_KEY = "e9ba398051fe48b9980ffecce4f555d5";
+    public static final String NEWS_API_BASE_URL = "https://newsapi.org/v2/";
 }
