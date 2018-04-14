66c256618207da4b88b889bc0ec99a20a1f06d3b
Kevin Araya <kevin.araya.camacho@est.una.ac.cr>
2018-04-14 15:55:23 -0600

    login y registro listos

--- app/src/main/java/com/example/csar/kbc_news/actividades/ActividadCuenta.java
+++ app/src/main/java/com/example/csar/kbc_news/actividades/ActividadCuenta.java
@@ -1,12 +1,34 @@
 package com.example.csar.kbc_news.actividades;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.example.csar.kbc_news.R;
+import com.example.csar.kbc_news.utils.VariablesGlobales;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class ActividadCuenta extends ActividadBase {
+    private FirebaseAuth mAuth = VariablesGlobales.getInstance().getmAuth();
+    private EditText emailEditText;
+    private EditText contrasenaEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +37,74 @@ public class ActividadCuenta extends ActividadBase {
         getLayoutInflater().inflate(R.layout.actividad_cuenta, contentFrameLayout);
         //NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         //navigationView.getMenu().getItem(0).setChecked(true);
+
+
+        TextView textoRegistrar = (TextView) findViewById(R.id.linkRegistrar);
+
+        textoRegistrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                Intent intento = new Intent(getApplicationContext(), ActividadRegistrar.class);
+                startActivity(intento);
+            }
+        });
+
+        emailEditText = (EditText) findViewById(R.id.campoCorreoLogin);
+        contrasenaEditText = (EditText) findViewById(R.id.campoContrasenaLogin);
+
+        Button botonLogin = findViewById(R.id.botonLogin);
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                login();
+            }
+        });
+    }
+
+    public void login() {
+        final ProgressDialog progressDialog  = new ProgressDialog(this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Verificando...");
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), contrasenaEditText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Intent intento = new Intent(getApplicationContext(), ActividadPrincipal.class);
+                            startActivity(intento);
+                        }
+
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof FirebaseAuthInvalidUserException ) {
+                    progressDialog.dismiss();
+                    emailEditText.setError("No existe cuenta con este correo");
+                }
+                if(e instanceof FirebaseAuthInvalidCredentialsException){
+                    progressDialog.dismiss();
+                    contrasenaEditText.setError("Contraseña incorrecta");
+
+                }
+            }
+        });
+    }
+
+    public void Mensaje(String msg) {
+        View v1 = getWindow().getDecorView().getRootView();
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(v1.getContext());
+        builder1.setMessage(msg);
+        builder1.setCancelable(true);
+        builder1.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
